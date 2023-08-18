b43c86bdcefe890267ecaf4ed5cbc9b90e0f8448
eks <kimzer@vista.aero>
2023-08-18 11:37:17 +0800

    Initial commit

---
+++ src/components/SystemRoleSettings.tsx
@@ -0,0 +1,91 @@
+import { Show, createSignal, createEffect } from 'solid-js'
+import IconEnv from './icons/Env'
+import IconX from './icons/X'
+import SettingsSlider from './SettingsSlider'
+import type { Accessor, Setter } from 'solid-js'
+
+interface Props {
+  canEdit: Accessor<boolean>
+  systemRoleEditing: Accessor<boolean>
+  setSystemRoleEditing: Setter<boolean>
+  currentSystemRoleSettings: Accessor<string>
+  setCurrentSystemRoleSettings: Setter<string>
+  temperatureSetting: (value: number) => void
+}
+
+export default (props: Props) => {
+  let systemInputRef: HTMLTextAreaElement
+  const [temperature, setTemperature] = createSignal(0.6)
+
+  const handleButtonClick = () => {
+    props.setCurrentSystemRoleSettings(systemInputRef.value)
+    props.setSystemRoleEditing(false)
+  }
+
+  createEffect(() => {
+    props.temperatureSetting(temperature())
+  })
+
+  return (
+    <div class="my-4">
+      <Show when={!props.systemRoleEditing()}>
+        <Show when={props.currentSystemRoleSettings()}>
+          <div>
+            <div class="fi gap-1 op-50 dark:op-60">
+              <Show when={props.canEdit()} fallback={<IconEnv />}>
+                <span onClick={() => props.setCurrentSystemRoleSettings('')} class="sys-edit-btn p-1 rd-50%" > <IconX /> </span>
+              </Show>
+              <span>系统规则：</span>
+            </div>
+            <div class="mt-1">
+              {props.currentSystemRoleSettings()}
+            </div>
+          </div>
+        </Show>
+        <Show when={!props.currentSystemRoleSettings() && props.canEdit()}>
+          <span onClick={() => props.setSystemRoleEditing(!props.systemRoleEditing())} class="sys-edit-btn">
+            <IconEnv />
+            <span>设置系统规则</span>
+          </span>
+        </Show>
+      </Show>
+      <Show when={props.systemRoleEditing() && props.canEdit()}>
+        <div>
+          <div class="text-sm fi gap-1 op-50 dark:op-60 mb-2">
+            <IconEnv />
+            <span>设置系统规则（AI的角色）：</span>
+          </div>
+          <div>
+            <textarea
+              ref={systemInputRef!}
+              placeholder="例如：你是医学领域的权威，总是详尽且简洁地回复人们的所有问题 ..."
+              autocomplete="off"
+              autofocus
+              rows="3"
+              gen-textarea
+            />
+          </div>
+          <div class="w-full fi fb">
+            <button class="text-sm whitespace-nowrap" onClick={handleButtonClick} gen-slate-btn>
+              设定
+            </button>
+            <div class="w-full ml-2">
+              <SettingsSlider
+                settings={{
+                  name: 'Temperature',
+                  type: 'slider',
+                  min: 0,
+                  max: 1,
+                  step: 0.01,
+                }}
+                editing={() => true}
+                value={temperature}
+                setValue={setTemperature}
+              />
+            </div>
+          </div>
+        </div>
+      </Show>
+    </div>
+  )
+}
